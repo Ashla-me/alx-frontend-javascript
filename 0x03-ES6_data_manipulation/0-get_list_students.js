b44d1cf275193349643c542b8442d678fcebd5f6
Ashla-me <mercyashamesunday@gmail.com>
2024-07-30 17:25:00 +0100

    first fix

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+/** @author Mercy */
+export default function getListStudents() {
+    /** @returns array of students */
+    return [
+      {id: 1, firstName: 'Guillaume', location: 'San Francisco'},
+      {id: 2, firstName: 'James', location: 'Columbia'},
+      {id: 5, firstName: 'Serena', location: 'San Francisco'},
+    ];
+  }
